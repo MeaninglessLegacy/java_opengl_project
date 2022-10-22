b96fe83df316939d596047f2be28e369db4c262a
Cameron Lee <cal26@sfu.ca>
2022-10-21 22:13:04 -0700

    implemented StatBlock class and created packages for the game engine and characters

---
+++ Game2D/src/main/java/org/group11/StatBlock.java
@@ -0,0 +1,25 @@
+package org.group11;
+
+public class StatBlock {
+
+    protected int lvl;
+    protected int exp;
+
+    protected int hp;
+    protected int maxHp;
+
+    protected int atk;
+
+    public int getLvl() { return this.lvl; }
+    public void setLvl(int lvl) { this.lvl = lvl; }
+    public int getExp() { return this.exp; }
+    public void setExp(int exp) { this.exp = exp; }
+
+    public int getHp() { return this.hp; }
+    public void setHp(int Hp) { this.hp = Hp; }
+    public int getMaxHp() { return this.maxHp; }
+    public void setMaxHp(int maxHp) { this.maxHp = maxHp; }
+
+    public int getAtk() { return this.atk; }
+    public void setAtk(int atk) { this.atk = atk; }
+}
