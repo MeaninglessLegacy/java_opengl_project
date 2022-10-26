18199d1d9dcf4aff53b6dfaad5b007d2670f2e91
Cameron Lee <cal26@sfu.ca>
2022-10-25 19:59:53 -0700

    added javadocs to Backpack class and changed removeItem() to move the last item of the array into the spot that the item being removed is in

--- Game2D/src/main/java/org/group11/items/Backpack.java
+++ Game2D/src/main/java/org/group11/items/Backpack.java
@@ -6,22 +6,31 @@ public class Backpack {
 
     protected ArrayList<Item> _inventory;
 
-    public ArrayList<Item> getItems() {
-        return this._inventory;
-    }
+    public ArrayList<Item> getItems() { return this._inventory; }
 
-    public void addItem (Item item) {
-        _inventory.add(item);
-    }
+    /**
+     * Adds an item to the backpack
+     *
+     * @param item item to add to backpack
+     */
+    public void addItem (Item item) { _inventory.add(item); }
 
-    public void removeItem (Item item) {
-        boolean searching = true;
+    /**
+     * Given an item, removes that type of item from the backpack if there is one
+     *
+     * @param item item to remove from backpack
+     * @return true if given item is found and removed, false if given item cannot be found
+     */
+    public boolean removeItem (Item item) {
         int i = 0;
-        while (i < _inventory.size() && searching) {
+        while (i < _inventory.size()) {
             if (_inventory.get(i) == item) {
                 _inventory.remove(i);
-                searching = false;
+                _inventory.add(_inventory.get(_inventory.size() - 1));
+                _inventory.remove(_inventory.size() - 1);
+                return true;
             }
         }
+        return false;
     }
 }
