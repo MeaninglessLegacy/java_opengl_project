8bdc4106f0446f1a4f9f6f2fe6a29024cf2ee8b1
Cameron Lee <cal26@sfu.ca>
2022-11-01 17:02:52 -0700

    added a line of walls on the left side of the test map

--- Dyskoles/src/main/java/org/group11/Packages/Game/Scripts/MapGenerators/SquareRoom.java
+++ Dyskoles/src/main/java/org/group11/Packages/Game/Scripts/MapGenerators/SquareRoom.java
@@ -6,6 +6,7 @@ import org.group11.Packages.Game.Scripts.Logic.Map;
 import org.group11.Packages.Game.Scripts.Logic.MapGenerator;
 import org.group11.Packages.Game.Scripts.Tile_Scripts.Floor;
 import org.group11.Packages.Game.Scripts.Tile_Scripts.Tile;
+import org.group11.Packages.Game.Scripts.Tile_Scripts.Wall;
 
 /**
  *
@@ -13,7 +14,7 @@ import org.group11.Packages.Game.Scripts.Tile_Scripts.Tile;
 public class SquareRoom extends MapGenerator {
     private Scene scene;
     /**
-     * Generates a 10x10 Square of floor tiles
+     * Generates a 10x10 Square of floor tiles, and adds a line of wall tiles along the left side
      * @return Map containing a 10x10 map of floor tiles
      */
     @Override
@@ -21,14 +22,21 @@ public class SquareRoom extends MapGenerator {
         scene = Scene.get_scene();
         if(scene == null) return null;
         Map newMap = new Map();
-        for(int i = 0; i< 10; i++){
-            for(int z = 0; z< 10; z++){
-                Vector3 pos = new Vector3(i,z,0);
+        for(int x = 0; x< 10; x++){
+            for(int y = 0; y< 10; y++){
+                Vector3 pos = new Vector3(x,y,0);
                 Tile newTile = new Floor();
                 newTile.transform.setPosition(pos);
                 newMap.setTile(pos, newTile);
                 scene.Instantiate(newTile);
             }
+        }
+        for (int y = -1; y < 11; y++) {
+            Vector3 pos = new Vector3(-1, y, 0);
+            Tile newTile = new Wall();
+            newTile.transform.setPosition(pos);
+            newMap.setTile(pos, newTile);
+            scene.Instantiate(newTile);
         }
         return newMap;
     }
