51ca2b5f0d8d74ce4746162c9e06c8dbc81f2482
Jimmy <jna94@sfu.ca>
2022-11-06 13:16:03 -0800

    fixed path finding for squareroom

--- Dyskoles/src/main/java/org/group11/Packages/Game/Scripts/Logic/Map.java
+++ Dyskoles/src/main/java/org/group11/Packages/Game/Scripts/Logic/Map.java
@@ -5,6 +5,7 @@ import org.group11.Packages.Core.Main.Scene;
 import org.group11.Packages.Game.Scripts.Tile_Scripts.Tile;
 
 
+import java.util.ArrayList;
 import java.util.Dictionary;
 import java.util.Enumeration;
 import java.util.Hashtable;
@@ -22,6 +23,8 @@ public class Map {
 
     private Scene scene;
 
+
+
     //******************************************************************************************************************
     //* methods
     //******************************************************************************************************************
@@ -30,7 +33,7 @@ public class Map {
      * @param pos Vector3 position to convert to a key.
      * @return The key mapped to by the Vector3 psoition.
      */
-    private String getPosKey(Vector3 pos){
+    public String getPosKey(Vector3 pos){
         return pos.x+""+pos.y+""+pos.z;
     }
 
@@ -74,30 +77,43 @@ public class Map {
         }
     }
 
-    public void getAlltile(){
+
+
+    //----------------------------Converting map to nodes----------------------------------------------------------------------
+
+
+    //node fields
+
+    final int maxCol = 100;
+    final int maxRow = 100;
+    Node [][] node = new Node [1000][1000];
+    Node startNode, goalNode, currentNode;
+    ArrayList<Node> openList = new ArrayList<>();
+    ArrayList<Node> checkedList = new ArrayList<>();
+    // OTHERS
+    boolean goalReached = false;
+    int step = 0;
+
+    Vector3 closestPosition = new Vector3();
+
+
+    public Vector3 getAlltile(Vector3 pointA, Vector3 pointB){
         scene = Scene.get_scene();
         int u =0;
         int t = 0;
         int[][] twoD_arr = new int[100][100];
-        Node [][] node = new Node [1000][1000];
+
+
 
 
         int col = 0;
         int row = 0;
-//        while(col < maxCol && row < maxRow) {
-//            node[col] [row] = new Node(col,row);
-//            this.add(node[col] [row]);
-//            col ++;
-//            if(col == maxCol) {
-//                col = 0;
-//                row ++;
-//            }
-//        }
 
 
 
 
-        if(scene == null) return;
+
+        if(scene == null) return pointA;
         for (Enumeration<String> tilePositions = _tileMap.keys(); tilePositions.hasMoreElements();) {
            // scene.Destroy(_tileMap.get(tilePositions.nextElement()));d
             String key = tilePositions.nextElement();
@@ -105,12 +121,7 @@ public class Map {
             System.out.println(_tileMap.get(key).getTileType()+" "+u);
 
 
-            //System.out.println((tilePositions.nextElement()));
-//            System.out.println(_tileMap.get((tilePositions.nextElement())).getTileType() +" "+u);   // this gets the tiletype from the map
-//            System.out.println((tilePositions.nextElement()));  // this getting all the vectors
-//            System.out.println((tilePositions.nextElement()));
-//
-//           System.out.println();
+
 //            u++;
             String[] res =(key.split("[.]", 0));
              //this can get me the x and y
@@ -144,7 +155,7 @@ public class Map {
             }
 
             // here can access the object
-
+            //this is setting up the
             if(_tileMap.get(key).getTileType() == Tile.tileTypes.floor){
             node[col] [row].solid= false;
             } else if(_tileMap.get(key).getTileType() == Tile.tileTypes.wall){
@@ -162,7 +173,175 @@ public class Map {
         }//end of for loop
 
 
+        // setting the origne
+        int Ax = (int) pointA.x;
+        int Ay = (int) pointA.y;
+        int Bx = (int) pointB.x;
+        int By = (int) pointB.y;
+
+        setStartNode(Ax,Ay);
+        setGoalNode(Bx,By);
+
+        setCostOnNodes();
+        autosearch();
+
+
+
+        return closestPosition;
+
+
+
+
+
+    }// end of getalltile
+
+    // this will set the ending position
+
+
+    private void setStartNode(int col, int row) {
+        node[col][row] .setAsStart();
+        startNode = node[col][row];
+        currentNode = startNode;
     }
+    private void setGoalNode(int col, int row) {
+        node[col][row] .setAsGoal();
+        goalNode = node[col] [row];
+
+    }
+
+
+
+    private void setCostOnNodes() {
+
+        int col1 = 0;
+        int row1 = 0;
+        while(col1 < 100 && row1 < 100) {
+            System.out.println(node[col1][row1].col +"  "+node[col1][row1].row );
+            getCost(node[col1][row1]);
+
+            col1 ++;
+            if(col1 == 100) {
+                col1 = 0;
+                row1 ++;
+            }
+        }
+    }
+
+
+
+    //  calculates the distance between start,current and goal nodes
+    private void getCost(Node node) {
+        // GET G COST (The distance from the start node)
+
+        int xDistance = Math.abs(node.col - startNode.col);
+        int yDistance = Math.abs(node.row - startNode.row);
+        node.gCost = xDistance + yDistance;
+        // GET H COST (The distance from the goal node).
+        xDistance = Math.abs(node.col - goalNode.col);
+        yDistance = Math.abs(node.row - goalNode.row);
+        node.hCost = xDistance + yDistance;
+        // GET F COST (The toal cost)
+
+        node.fCost = node.gCost + node.hCost;
+
+        System.out.println(node.fCost + " G cost "+node.gCost );
+
+    }
+
+
+
+
+    private void openNode(Node node) {
+        if(node.open == false && node.checked == false && node.solid == false) {
+            // If the node is not opened yet, add it to the open list.
+            node.setAsOpen();
+            node.parent = currentNode;
+            openList.add(node);
+        }
+    }
+
+
+    // this will autosearch the path automaticlay
+    public void autosearch() {
+
+        while(goalReached == false) {
+            int col = currentNode.col;
+
+            int row = currentNode.row;
+            currentNode.setAsChecked(); // this will check each node
+            checkedList.add(currentNode); //
+            openList.remove(currentNode);
+
+
+            // OPEN THE UP NODE
+            if(row -1 >= 0) {
+                openNode(node[col] [row-1]);
+            }
+            // OPEN THE LEFT NODE
+            if(col -1 >= 0) {
+                openNode(node[col-1] [row]);
+            }
+            // OPEN THE DOWN NODE
+            if(row +1 < maxRow) {
+                openNode(node[col] [row+1]);
+            }
+            // OPEN THE RIGHT NODE
+            if(col +1 < maxCol) {
+                openNode(node[col+1] [row]);
+            }
+
+            // FIND THE BEST NODE
+            int bestNodeIndex = 0;
+            int bestNodefCost = 999;
+            for(int i= 0; i < openList.size() ; i ++) {
+                // Check if this node's F cost is better
+                if(openList.get (i) .fCost < bestNodefCost) {
+                    bestNodeIndex=i;
+                    bestNodefCost = openList.get(i).fCost;
+                }
+                // If F cost is equal, check the G cost
+                else if(openList.get(i) .fCost == bestNodefCost) {
+                    if(openList.get(i) .gCost < openList.get(bestNodeIndex).gCost) {
+                        bestNodeIndex = i;
+                    }
+                }
+            }
+
+
+            // After the loop, we get the best node which is our next step
+            currentNode = openList.get(bestNodeIndex);
+            if(currentNode == goalNode) {
+                goalReached = true;
+                trackThePath();
+            }
+
+
+        }
+        step++;
+    }
+
+    private void trackThePath() {
+
+        // Backtrack and draw the best path
+        Node current = goalNode;
+        while(current != startNode) {
+            current = current.parent;
+            if(current.parent == startNode) {
+                System.out.println( current.row+" closest "+current.col);
+               Vector3 newvec = new Vector3(current.row,current.col,0);
+               closestPosition = newvec;
+
+
+
+            }
+
+
+        }
+        //return null;
+    }
+
+
+
 
 
 
