c9b3ce5d1ccd1bf716684b107297cbd4b39fc1c5
Cameron Lee <cal26@sfu.ca>
2022-11-13 12:03:31 -0800

    made directories for testing

---
+++ Dyskoles/src/test/java/org/group11/Packages/Game/Scripts/Logic/GameLogicDriverTest.java
@@ -0,0 +1,18 @@
+package org.group11.Packages.Game.Scripts.Logic;
+import org.group11.Packages.Game.Scripts.Logic.GameLogicDriver;
+import org.junit.Before;
+import org.junit.Test;
+
+public class GameLogicDriverTest {
+    private GameLogicDriver GLD;
+
+    @Before
+    void setup() {
+        GLD = GameLogicDriver.getInstance();
+    }
+
+    @Test
+    public void loadNewLevelTest() {
+
+    }
+}
