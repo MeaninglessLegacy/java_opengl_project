374b793188f0f66060a8eba441f7ea6fa926ebfd
Cameron Lee <cal26@sfu.ca>
2022-11-19 20:50:38 -0800

    changed the storage for all the sprites to a hashmap

--- Dyskoles/src/main/java/org/group11/Packages/Game/Scripts/Logic/MenuScreen.java
+++ Dyskoles/src/main/java/org/group11/Packages/Game/Scripts/Logic/MenuScreen.java
@@ -2,16 +2,22 @@ package org.group11.Packages.Game.Scripts.Logic;
 
 import org.group11.Packages.Core.Main.GameObject;
 import org.group11.Packages.Core.Main.Scene;
-import org.group11.Packages.Game.Scripts.UI_Scripts.Menu_Scripts.GameOver;
+import org.group11.Packages.Game.Scripts.UI_Scripts.Menu_Scripts.*;
+import org.group11.Packages.Game.Scripts.UI_Scripts.Menu_Scripts.PressSpaceSprites.SpaceNextLevel;
 import org.group11.Packages.Game.Scripts.UI_Scripts.Menu_Scripts.PressSpaceSprites.SpaceRetry;
+import org.group11.Packages.Game.Scripts.UI_Scripts.Menu_Scripts.PressSpaceSprites.SpaceStartGame;
+import org.group11.Packages.Game.Scripts.UI_Scripts.Menu_Scripts.PressSpaceSprites.SpaceStartOver;
+
+import java.util.HashMap;
+import java.util.Set;
 
 public class MenuScreen extends GameObject {
     //******************************************************************************************************************
     //* variables
     //******************************************************************************************************************
     private Scene scene;
-    private final GameOver gameOverSprite;
-    private final SpaceRetry spaceRetrySprite;
+
+    HashMap<String, MenuElement> menuElements = new HashMap<>();
 
     private boolean onMenu;
 
@@ -19,16 +25,48 @@ public class MenuScreen extends GameObject {
     //* constructor
     //******************************************************************************************************************
     public MenuScreen() {
-        gameOverSprite = new GameOver();
-        spaceRetrySprite = new SpaceRetry();
+        menuElements.put("menuBackground", new MenuBackground());
+        menuElements.put("Instructions", new Instructions());
+        menuElements.put("youWin", new YouWin());
+        menuElements.put("exitReached", new ExitReached());
+        menuElements.put("gameOver", new GameOver());
+        menuElements.put("healthReduced0", new HealthReduced0());
+        menuElements.put("spaceStartGame", new SpaceStartGame());
+        menuElements.put("spaceNextLevel", new SpaceNextLevel());
+        menuElements.put("spaceRetry", new SpaceRetry());
+        menuElements.put("spaceStartOver", new SpaceStartOver());
     }
 
     //******************************************************************************************************************
     //* methods
     //******************************************************************************************************************
-    public void createMenu() {
-        scene.Instantiate(gameOverSprite);
-        scene.Instantiate(spaceRetrySprite);
+    private void createTitleScreen() {
+
+    }
+
+    public void createMenu(boolean won, int _gameStage, int numberOfLevels) {
+        /*
+        if (won) {
+            scene.Instantiate(menuElements.get("youWin"));
+            scene.Instantiate(menuElements.get("exitReached"));
+            if (_gameStage < numberOfLevels) {
+                scene.Instantiate(menuElements.get("spaceNextLevel"));
+            }
+            else {
+                scene.Instantiate(menuElements.get("spaceStartOver"));
+            }
+        }
+        else {
+            scene.Instantiate(menuElements.get("gameOver"));
+            scene.Instantiate(menuElements.get("healthReduced0"));
+            scene.Instantiate(menuElements.get("spaceRetry"));
+        }
+        */
+
+
+
+        scene.Instantiate(menuElements.get("gameOver"));
+        scene.Instantiate(menuElements.get("spaceRetry"));
     }
 
     //******************************************************************************************************************
@@ -44,7 +82,7 @@ public class MenuScreen extends GameObject {
         scene = Scene.get_scene();
         onMenu = true;
 
-        createMenu();
+        createMenu(true, 1, 1);
     }
 
     @Override
@@ -60,7 +98,9 @@ public class MenuScreen extends GameObject {
 
     @Override
     public void Delete() {
-        scene.Destroy(gameOverSprite);
-        scene.Destroy(spaceRetrySprite);
+        Set<String> toDelete = menuElements.keySet();
+        for (String s : toDelete) {
+            scene.Destroy(menuElements.get(s));
+        }
     }
 }
