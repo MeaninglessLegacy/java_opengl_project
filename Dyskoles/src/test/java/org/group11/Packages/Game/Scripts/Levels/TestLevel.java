fb30ad9726d8a4145a680bc0b42c1fa176a5b214
Cameron Lee <cal26@sfu.ca>
2022-11-21 16:51:08 -0800

    removed all characters from level as they will be added in individual tests

--- Dyskoles/src/test/java/org/group11/Packages/Game/Scripts/Levels/TestLevel.java
+++ Dyskoles/src/test/java/org/group11/Packages/Game/Scripts/Levels/TestLevel.java
@@ -10,9 +10,5 @@ import org.group11.Packages.Game.Scripts.MapGenerators.TestArea;
 public class TestLevel extends Level {
     public TestLevel() {
         _mapGenerator = new TestArea();
-
-        _players.add(new MainCharacter(new Vector3(0, 2, 0)));
-        _enemies.add(new Minion(new Vector3(0, 3, 0)));
-        _items.add(new Key(new Vector3(1,2,0)));
     }
 }
